cfc83ce4045cdb9f9d877fbbacf3576e84b71e2e
chrisr04 <you@example.com>
2019-07-20 14:15:47 -0500

    create database and config

--- djanblog/settings.py
+++ djanblog/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'djanblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djanblog',
+        'USER':'root',
+        'PASSWORD':'1083031426Mr)',
+        'PORT':'3306',
     }
 }
 
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
 TIME_ZONE = 'UTC'
 
